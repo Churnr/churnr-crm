2559f0d9ef4f01e032a7e2f6bf491bdf15227610
Christoffer Foldager <58327723+Foldager94@users.noreply.github.com>
2022-03-24 14:31:04 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,25 +12,6 @@ import { getAuth} from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDIu5UUR1E8Slx-Iwt6bwp9tjgLcwBdhU8",
-
-  authDomain: "churnr-system.firebaseapp.com",
-
-  projectId: "churnr-system",
-
-  storageBucket: "churnr-system.appspot.com",
-
-  messagingSenderId: "679339937318",
-
-  appId: "1:679339937318:web:13eb0a3e1e41414d4be0e7",
-
-  measurementId: "G-Q96LHT7WHS"
-
-};
-
-
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
