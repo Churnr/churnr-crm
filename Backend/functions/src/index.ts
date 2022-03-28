becfb452b71920da66f62a3357b0a95e90b075e0
Christoffer Kofoed Foldager <c.k.foldager@gmail.com>
2022-03-28 13:25:27 +0200

    fetchDunningInvoices, stage 1 complete

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -2,34 +2,57 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin"
 import fetch from "node-fetch";
 // import { doc, setDoc } from "firebase/firestore"; 
-function testing_firebase_scope() {
-    return console.log("Working")
-}
 admin.initializeApp();
 
-exports.fetchInvoices = functions.https.onRequest(async (req, res) => {
+type options = {
+    "method": string,
+    "headers": headers,
+    "json": boolean
+};
+type headers = {
+    "Content-Type": string,
+    "Authorization": string
+}
 
-    const _url = 'https://api.reepay.com/v1/list/invoice?size=10&state=dunning'
-    const _headers = {
+
+async function testing_firebase_scope(url:string, options:options, next_page_token:string="", return_array=[]):Promise<any> {
+    const response:any = await (await fetch(url+next_page_token, options)).json()
+    return_array = return_array.concat(response.content)
+    if(response.next_page_token != undefined){
+        return testing_firebase_scope(url, options, "&next_page_token="+response.next_page_token, return_array)
+    }
+    return return_array   
+}
+
+
+
+// FUNCTION #1 Filtering and checking - scheduler
+//      1. fetch API data - DONE
+//      2. filtrér data (invoice id)
+//      3. Check if same customer, but different invoice ids
+//      4. Cross reference with Current invoice collection and Reepay API
+//      5. Save remaining data
+exports.fetchDunningInvoices = functions.https.onRequest(async (req, res) => {
+    const _url = 'https://api.reepay.com/v1/list/invoice?size=100&state=dunning'
+
+    const headers:headers = {
         'Content-Type': 'application/json',
         'Authorization': `Basic ${process.env.API_KEY_LALATOYS}`
     }
 
-    const options = {
+    const options:options = {
         method: 'GET',
-        headers: _headers,
+        headers: headers,
         json: true
     };
 
-    await fetch(_url, options)
-    .then(respons => respons.json())
-    .then(respons => {  
-        const ress:any = respons
-        testing_firebase_scope()
-        console.log(ress.content)
-    });
+    const content_array:Array<any> = await testing_firebase_scope(_url, options)
+    // const writeResult = await admin.firestore().collection('ActivDunning').add(content_array)
+    for (let dunningInvoices of content_array){
+        await admin.firestore().collection('ActivDunning').add(dunningInvoices);
+      }
 
-    res.json({result: `Message with ID: added.`});
+    res.json({result: `Message with ID: ${content_array} added.`});
   });
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -39,12 +62,6 @@ exports.fetchInvoices = functions.https.onRequest(async (req, res) => {
    response.send("Hello from");
 });
 
-// FUNCTION #1 Filtering and checking - scheduler
-//      1. fetch API data
-//      2. filtrér data (invoice id)
-//      3. Check if same customer, but different invoice ids
-//      4. Cross reference with Current invoice collection and Reepay API
-//      5. Save remaining data
 
 // F#1 -> F#2
 // FUNCTION #2 Updating the active dunning list (not failed) - scheduler
