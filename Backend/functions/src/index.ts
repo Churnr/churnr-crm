065690d8d96b59a0e7e6d3f77e73edaadbeb0deb
Christoffer Kofoed Foldager <c.k.foldager@gmail.com>
2022-04-07 10:14:18 +0200

    jobs done

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -4,59 +4,9 @@ import * as admin from "firebase-admin";
 import * as customType from "../types/types";
 import * as firestoreUtils from "../utils/firestoreUtils";
 import * as httpUtils from "../utils/httpUtils";
-import * as cors from "cors";
 admin.initializeApp();
 
-const corsHandler = cors({
-  origin: [
-    'http://localhost:3000',
-  ],
-})
 
-
-// const validateFirebaseIdToken = async (req:any, res:any, next:any) => {
-//   functions.logger.log("Check if request is authorized with Firebase ID token");
-
-//   if ((!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) &&
-//       !(req.cookies && req.cookies.__session)) {
-//     functions.logger.error(
-//         "No Firebase ID token was passed as a Bearer token in the Authorization header.",
-//         "Make sure you authorize your request by providing the following HTTP header:",
-//         "Authorization: Bearer <Firebase ID Token>",
-//         "or by passing a \"__session\" cookie."
-//     );
-//     res.status(403).send("Unauthorized");
-//     return;
-//   }
-
-//   let idToken;
-//   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
-//     functions.logger.log("Found \"Authorization\" header");
-//     // Read the ID Token from the Authorization header.
-//     idToken = req.headers.authorization.split("Bearer ")[1];
-//   } else if (req.cookies) {
-//     functions.logger.log("Found \"__session\" cookie");
-//     // Read the ID Token from cookie.
-//     idToken = req.cookies.__session;
-//   } else {
-//     // No cookie
-//     res.status(403).send("Unauthorized");
-//     return;
-//   }
-
-//   try {
-//     const decodedIdToken = await admin.auth().verifyIdToken(idToken);
-//     functions.logger.log("ID Token correctly decoded", decodedIdToken);
-//     req.user = decodedIdToken;
-//     next();
-//     return;
-//   } catch (error) {
-//     functions.logger.error("Error while verifying Firebase ID token:", error);
-//     res.status(403).send("Unauthorized");
-//     return;
-//   }
-// };
-// app.use(validateFirebaseIdToken);
 
 // const db = getFirestore(app);
 // const citiesRef = collection(db, "cities");
@@ -99,78 +49,3 @@ functions.region("europe-west2").pubsub.schedule("0 23 * * *")
     });
 
 
-export const helloWorld = functions.https.onRequest(async (req, res) => {
-  corsHandler(req, res, async () => {
-    try {
-      const token:any = req.headers.authorization
-      const userData:any = await (await admin.auth().verifyIdToken(token)).uid;
-      console.log(userData) // here we have uid of verified user
-      console.log(token)
-      const invoiceIdArray:Array<string> = [];
-      const dbInvoice = await admin.firestore()
-          .collection("test").listDocuments();
-      for (const s of dbInvoice) {
-        const QueryDocumentSnapshot = await s.get();
-        const data: any = QueryDocumentSnapshot.data();
-        const dataID: string = data.test; 
-        console.log(dataID)
-        invoiceIdArray.push(dataID);
-      }
-      // check user access or do whatever we need here
-      res.json(invoiceIdArray);
-    } catch (e) {
-      console.log("NOT ALLOWED MYMAN")
-      res.status(403).json("NOT ALLOWED MYMAN")
-    }
-  })
-});
-
-
-export const helloWorld2 = functions.https.onRequest(async (req, res) => {
-  const invoiceIdArray:Array<string> = [];
-  const dbInvoice = await admin.firestore()
-      .collection("tes").listDocuments();
-  for (const s of dbInvoice) {
-    const QueryDocumentSnapshot = await s.get();
-    const data: any = QueryDocumentSnapshot.data();
-    const dataID: string = data.id;
-    invoiceIdArray.push(dataID);
-  }
-
-
-
-
-     // check user access or do whatever we need here
-     const jsonExample = {
-       "essential": [
-         "store",
-         -1509028413.9262555,
-         "perfectly",
-         [
-           -1808610733,
-           false,
-           "keep",
-           -1020454813,
-           false,
-           "movie",
-         ],
-         true,
-         {
-           "further": "consider",
-           "policeman": "red",
-           "concerned": "alphabet",
-           "passage": "one",
-           "wave": false,
-           "picture": 1698654544.095453,
-         },
-       ],
-       "ability": "other",
-       "policeman": 31538672,
-       "development": 765898294,
-       "no": true,
-       "occasionally": "exchange",
-     };
-     res.json(jsonExample);
-
- });
- 
