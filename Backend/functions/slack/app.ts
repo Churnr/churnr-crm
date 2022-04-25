23eec04c5fe4245e657a433be61c73d073ce687c
foldager94 <c.k.foldager@gmail.com>
2022-04-25 14:03:20 +0200

    added halloworld to functions

---
+++ Backend/functions/slack/app.ts
@@ -0,0 +1,37 @@
+import * as slack from "@slack/web-api";
+
+// Read a token from the environment variables
+const token = process.env.SLACK_TOKEN;
+
+// Initialize
+const web = new slack.WebClient(token);
+
+
+const conversationId = "C03BZ4Z446A";
+
+(async () => {
+  // Post a message to the channel, and await the result.
+  // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
+  const result = await web.chat.postMessage({
+    text: "Hello world!",
+    channel: conversationId,
+  });
+
+  // The result contains an identifier for the message, `ts`.
+  console.log(`Successfully send message ${result.ts}
+   in conversation ${conversationId}`);
+})();
+
+/**
+ * Gets invoice ids from invoice collection from firestore
+ * and push it to array of strings - invoiceIdArray
+ * @param {string} text
+ * @param {string} channel
+ * @return {Array<string>} Array of invoice ids
+ */
+export async function publishMessage(text:string, channel:string) {
+  const result = await web.chat.postMessage({
+    text: "Hello world!",
+    channel: conversationId,
+  });
+}
--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -6,6 +6,7 @@ import * as firestoreUtils from "../utils/firestoreUtils";
 import * as httpUtils from "../utils/httpUtils";
 // import * as cors from "cors";
 // import * as middleware from "../middleware/middleware";
+import * as slackApp from "../slack/app";
 import * as express from "express";
 const app = express();
 admin.initializeApp();
@@ -15,38 +16,6 @@ admin.initializeApp();
 // app.use(cors(options2));
 app.use(express.json());
 // app.use(middleware.validateFirebaseIdToken);
-
-// functions.region("europe-west2").pubsub.schedule("0 23 * * *")
-//     .timeZone("Europe/Copenhagen").onRun(async (context) => {
-// const _url = "https://api.reepay.com/v1/list/invoice?size=100&state=dunning";
-
-// const headers: customType.headers = {
-//   "Content-Type": "application/json",
-//   "Authorization": `Basic ${process.env.API_KEY_LALATOYS}`,
-// };
-
-// const options: customType.options = {
-//   method: "GET",
-//   headers: headers,
-//   json: true,
-// };
-
-// const invoiceIdArray =
-// await firestoreUtils.getInvoiceIdsFromCollection("ActiveDunning");
-
-// const contentArray: Array<any> =
-// await httpUtils.retriveReepayList(_url, options);
-
-// for (const dunningInvoices of contentArray) {
-//   if (invoiceIdArray.indexOf(dunningInvoices.id) == -1) {
-//     await admin.firestore().collection("ActivDunning")
-//         .add(dunningInvoices);
-//   }
-// }
-
-//       return null;
-//     });
-
 
 app.get("/getdunning", async (req, res) => {
   const customers:any = await firestoreUtils.getCustomers();
@@ -112,7 +81,9 @@ app.post("/createcustomer", async (req, res) => {
 });
 
 app.post("/halloworld", async (req, res) => {
-  functions.logger.log("Request", req);
-  res.status(200).send("ay okay");
+  const result = slackApp.publishMessage("Hallo World", "C03BZ4Z446A");
+  functions.logger.log("something", result);
 });
 exports.app = functions.https.onRequest(app);
+
+
