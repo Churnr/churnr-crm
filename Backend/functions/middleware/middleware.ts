2902877b2a325986488841f9fd8ff37729feec66
foldager94 <c.k.foldager@gmail.com>
2022-04-25 23:23:57 +0200

    hmmmm

--- Backend/functions/middleware/middleware.ts
+++ Backend/functions/middleware/middleware.ts
@@ -88,15 +88,15 @@ export async function validateIpAddress(req:any, res:any, next:any) {
  */
 export function validateSlackSigningSecret(req:any, res:any, next:any) {
   const slackSigningSecret = process.env.SLACK_SIGNING_SECRET!;
-
   const requestSignature = req.headers["x-slack-signature"] as string;
   const requestTimestamp = req.headers["x-slack-request-timestamp"];
-
+  const requestBody = req.body();
+  
   const hmac = crypto.createHmac("sha256", slackSigningSecret);
 
   try {
     const [version, hash] = requestSignature.split("=");
-    const base = `${version}:${requestTimestamp}:${JSON.stringify(req.body)}`;
+    const base = `${version}:${requestTimestamp}:${req.body}`;
     hmac.update(base);
     functions.logger.log("hmac", hmac.digest("hex"));
     functions.logger.log("hash", hash);
