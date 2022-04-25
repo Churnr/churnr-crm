4e195cc15f749f82d25a6fbb8def17a2d0c9a02b
foldager94 <c.k.foldager@gmail.com>
2022-04-25 15:08:46 +0200

    removed not needed code

--- Backend/functions/slack/app.ts
+++ Backend/functions/slack/app.ts
@@ -1,26 +1,10 @@
-import * as slack from "@slack/web-api";
-
+import {WebClient} from "@slack/web-api";
 // Read a token from the environment variables
 const token = process.env.SLACK_TOKEN;
 
 // Initialize
-const web = new slack.WebClient(token);
+const web = new WebClient(token);
 
-
-const conversationId = "C03BZ4Z446A";
-
-(async () => {
-  // Post a message to the channel, and await the result.
-  // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-  const result = await web.chat.postMessage({
-    text: "Hello world!",
-    channel: conversationId,
-  });
-
-  // The result contains an identifier for the message, `ts`.
-  console.log(`Successfully send message ${result.ts}
-   in conversation ${conversationId}`);
-})();
 
 /**
  * Gets invoice ids from invoice collection from firestore
@@ -30,8 +14,6 @@ const conversationId = "C03BZ4Z446A";
  * @return {Array<string>} Array of invoice ids
  */
 export async function publishMessage(text:string, channelId:string) {
-  const token = process.env.SLACK_TOKEN;
-  const web = new slack.WebClient(token);
   const result = await web.chat.postMessage({
     token: token,
     text: text,
@@ -39,3 +21,5 @@ export async function publishMessage(text:string, channelId:string) {
   });
   return result;
 }
+
+
