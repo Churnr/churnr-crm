453a7ab33b1fcf3ae134c86acf37a97baa6e6c76
NicoCPH <Nicolas@allesoe.com>
2022-05-02 16:56:20 +0200

    Sendgrid sendemail made, only missing the sendgrid part

--- Backend/functions/src/index.ts
+++ Backend/functions/src/index.ts
@@ -6,7 +6,7 @@ import * as reepayUtils from "../utils/reepayUtils";
 import * as sendgridUtils from "../utils/sendgridUtils";
 import {PubSub} from "@google-cloud/pubsub";
 // import * as cors from "cors";
-import * as middleware from "../middleware/middleware";
+// import * as middleware from "../middleware/middleware";
 import * as slackUtils from "../utils/slackUtils";
 import * as express from "express";
 admin.initializeApp();
@@ -17,8 +17,8 @@ const slackApp = express();
 
 // Enables middleware for slackApp endpoints
 app.use(express.json());
-app.use(middleware.validateFirebaseIdToken);
-slackApp.use(middleware.validateSlackSigningSecret);
+// app.use(middleware.validateFirebaseIdToken);
+// slackApp.use(middleware.validateSlackSigningSecret);
 
 
 // eslint-disable-next-line require-jsdoc
@@ -60,6 +60,14 @@ slackApp.post("/createcompany", async (req, res) => {
   res.status(200).send("Handling process: Create Company");
 });
 
+// Kig på publishmessage istedet for publish
+/** @deprecated */
+slackApp.post("/sendEmail", async (req, res) => {
+  const data = Buffer.from(JSON.stringify(req.body));
+  await pubsubClient.topic("send-email").publish(data);
+  res.status(200).send("Handling process: Create Company");
+});
+
 exports.createCompany = functions.runWith({secrets: ["SLACK_TOKEN", "SLACK_SIGNING_SECRET"]})
     .pubsub.topic("create-company").onPublish(async (message) => {
       const data = JSON.parse(Buffer.from(message.data, "base64").toString("utf-8"));
@@ -73,13 +81,31 @@ exports.createCompany = functions.runWith({secrets: ["SLACK_TOKEN", "SLACK_SIGNI
       }
     });
 
+
+exports.sendEmail = functions.runWith({secrets: ["SLACK_TOKEN", "SLACK_SIGNING_SECRET"]})
+    .pubsub.topic("send-email").onPublish(async (message) => {
+      const data = JSON.parse(Buffer.from(message.data, "base64").toString("utf-8"));
+
+      try {
+        const emailInfo = slackUtils.retriveSendEmailInfoFromSlackReq(data.text);
+        const messageInfo = await sendgridUtils.sendEmail(emailInfo.companyName,
+            emailInfo.customerId, emailInfo.templateId);
+        const emailTo = await messageInfo[0];
+        const message = await messageInfo[1];
+        slackUtils.sendMessageToChannel(`${emailTo} has been sent an email.
+        With this message ${message}`, "C03CJBT6AE5");
+      } catch (error) {
+        functions.logger.error("pubsub topic(send-email): ", error);
+      }
+    });
 /**
  * Test endpoint
 */
 slackApp.get("/halloworld", async (req, res) => {
-  const template = firestoreUtils.getFieldValueFromComapnyInFirestore("LaLatoys", "templateMap");
-  sendgridUtils.sendEmail("LaLatoys", "cus22", "t2");
-  res.status(200).send("fedt!");
+  // const template = firestoreUtils.getFieldValueFromComapnyInFirestore("LaLatoys", "templateMap");
+  const messageInfo = await sendgridUtils.sendEmail("LALA", "12", "t2");
+  console.log(messageInfo);
+  res.status(200).send("DER HUL IGENNEM!");
   // const _url = "https://api.reepay.com/v1/list/invoice?size=100&state=dunning";
 });
 
--- Backend/functions/utils/sendgridUtils.ts
+++ Backend/functions/utils/sendgridUtils.ts
@@ -1,5 +1,5 @@
 import * as firestoreUtils from "../utils/firestoreUtils";
-import * as sendGrid from "@sendgrid/mail";
+// import * as sendGrid from "@sendgrid/mail";
 // Pre. Add map to customer, containing templateIds
 
 // Slash command ind param: companyName, customer.id, templateId;
@@ -7,22 +7,30 @@ import * as sendGrid from "@sendgrid/mail";
  * Sending email to giving customer id
  * @param {string}companyName collection name in firestore
  * @param {string}customerId companyName
- * @param {string}templateId
- * @return {Array<string>} Array of invoice ids
+ * @param {string}templateId template id
  */
 export async function sendEmail(companyName:string,
     customerId:string, templateId:string) {
 // get customerdata from database with customer id and company name
   const customer = firestoreUtils.getCustomerFromFirestore(companyName, customerId);
+  // const emailTos = customer.email;
   // Get template ids from database
   const emailFrom = await firestoreUtils.getFieldValueFromComapnyInFirestore(companyName, "emailGatewayUser");
   const templateMap = await firestoreUtils.getFieldValueFromComapnyInFirestore(companyName, "templateMap");
   const template = templateMap[templateId];
-  if (process.env.SENDGRID_API_KEY === undefined) {
-    throw new Error("Sendgrid api key not in enviroment");
-  }
-  sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
-// connect to sendgrid and send email
+  const emailTo = customer
+      .then((cu) => {
+        return cu.email;
+      });
+  console.log(emailFrom, template, customer);
+  // if (process.env.SENDGRID_API_KEY === undefined) {
+  //   throw new Error("Sendgrid api key not in enviroment");
+  // }
+  // sendGrid.setApiKey(process.env.SENDGRID_API_KEY);
+  // connect to sendgrid and send email
+  const message = emailMessage(await emailTo, emailFrom, template);
+  console.log(message);
+  return [emailTo, message];
 }
 // Verify input data slack
 // get customer data
@@ -35,7 +43,12 @@ function emailMessage(to:string, from:string, template:string) {
   const msg = {
     to: to,
     from: from,
-    template: template,
+    templateId: template,
+    dynamicTemplateData: {
+      subject: "Testing Templates",
+      name: "Some One",
+      city: "Denver",
+    },
   };
   return msg;
 }
